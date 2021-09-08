87da310abf1e4bc1e8c1a1103d0d409eeb9cacb6
jennifergoldberg <jagoldbe@gmail.com>
2021-09-08 19:54:54 -0400

    add dogs view and tried to get bulma working added bootstrap instead

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-from .views import Home 
+from .views import Home, DogView, Signup
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
+  path('dogs/', DogView.as_view(), name='dogs_view'),
+  path('accounts/signup', Signup.as_view(), name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,9 @@ class Home(TemplateView):
         context = super().get_context_data(**kwargs)
         return context
 
+class DogView(TemplateView):
+    template_name = 'dogs_view.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
