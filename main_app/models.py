b94a5dc27dcc5620466ca6fa554b861a7522fb56
jennifergoldberg <jagoldbe@gmail.com>
2021-09-09 17:27:58 -0400

    updated models

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
-from django.db.models.fields import DateTimeField, TextField, BooleanField
+from django.db.models.fields import DateTimeField, TextField, BooleanField, CharField
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -9,10 +9,10 @@ from django.contrib.auth.models import User
 
 class Profile(Model):
   user = models.OneToOneField(User, on_delete=CASCADE, related_name='profile')
-  avatar = TextField(max_length=500, blank=True)
+  avatar = CharField(max_length=500, blank=True)
   bio = TextField(max_length=2500, blank=True)
-  location = TextField(max_length=500)
-  org_name = TextField(max_length=500, blank=True)
+  location = CharField(max_length=150)
+  org_name = CharField(max_length=150, blank=True)
   verified = BooleanField(default=False)
 
   def __str__(self):
@@ -21,20 +21,20 @@ class Profile(Model):
 
 class Post(Model):
   user = models.ForeignKey(User, on_delete=CASCADE, related_name='post')
-  dog_name = TextField(max_length=500, blank=False)
-  image = TextField(max_length=500, blank=False)
-  image_two = TextField(max_length=500, blank=True)
-  image_three = TextField(max_length=500, blank=True)
+  dog_name = CharField(max_length=150, blank=False)
+  image = CharField(max_length=500, blank=False)
+  image_two = CharField(max_length=500, blank=True)
+  image_three = CharField(max_length=500, blank=True)
   bio = TextField(max_length=2500, blank=False)
-  color = TextField(max_length=500, blank=True)
-  gender = TextField(max_length=500, blank=True)
-  friendly = TextField(max_length=500, blank=True)
-  kids = TextField(max_length=500, blank=True)
-  age = TextField(max_length=500, blank=True)
-  breed = TextField(max_length=500, blank=True)
-  size = TextField(max_length=500, blank=True)
-  health = TextField(max_length=500, blank=True)
-  active = TextField(max_length=500, blank=True)
+  color = CharField(max_length=150, blank=True)
+  gender = CharField(max_length=150, blank=True)
+  friendly = CharField(max_length=200, blank=True)
+  kids = CharField(max_length=200, blank=True)
+  age = CharField(max_length=150, blank=True)
+  breed = CharField(max_length=200, blank=True)
+  size = CharField(max_length=300, blank=True)
+  health = CharField(max_length=200, blank=True)
+  active = CharField(max_length=200, blank=True)
   house_trained = BooleanField(default=False, blank=True)
   available = BooleanField(default=False, blank=False)
   created_at = DateTimeField(auto_now=False, auto_now_add=False)
@@ -49,7 +49,7 @@ class Post(Model):
 class Comment(Model):
   user = models.ForeignKey(User, on_delete=CASCADE, related_name='comment')
   post = models.ForeignKey(Post, on_delete=CASCADE, related_name='comment')
-  content = TextField(max_length=2500)
+  content = TextField(max_length=1000)
   created_at = DateTimeField(auto_now=False, auto_now_add=False)
 
   def __str__(self):
