292258dfdfefb763d79f29eb8caddb3ad8d06438
jennifergoldberg <jagoldbe@gmail.com>
2021-09-10 18:07:32 -0400

    styling home page, add post model to home in views

--- main_app/views.py
+++ main_app/views.py
@@ -14,9 +14,11 @@ from .models import User, Post, Comment, Profile
 
 class Home(TemplateView):
     template_name = 'home.html'
+    model=Post
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.all
         return context
 
 class DogView(TemplateView):
