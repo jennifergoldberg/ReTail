c2f7bc63619573797d4d6f423e817e34f4d89ad7
jennifergoldberg <jagoldbe@gmail.com>
2021-09-12 14:21:11 -0400

    updated dog view

--- main_app/views.py
+++ main_app/views.py
@@ -26,6 +26,7 @@ class DogView(TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.all
         return context
 
 class Signup(View):
