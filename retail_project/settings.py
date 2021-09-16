a54c4cad9e37723571a08c99ae66db68155d24f8
jennifergoldberg <jagoldbe@gmail.com>
2021-09-16 14:49:52 -0400

    secret in settings.py

--- retail_project/settings.py
+++ retail_project/settings.py
@@ -13,11 +13,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 import environ
 
-env = environ.Env(
-    DEBUG=(bool, False)
-)
+env = environ.Env()
 # reading .env file
 environ.Env.read_env()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
 
 from pathlib import Path
 
@@ -28,8 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
