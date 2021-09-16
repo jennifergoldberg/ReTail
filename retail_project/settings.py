282d20d7375a518174008672235c7e6126f7b894
jennifergoldberg <jagoldbe@gmail.com>
2021-09-16 14:39:24 -0400

    deploy take two

--- retail_project/settings.py
+++ retail_project/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-import os
-import environ
+# import os
+# import environ
+from os import environ
 env = environ.Env(
     DEBUG=(bool, False)
 )
